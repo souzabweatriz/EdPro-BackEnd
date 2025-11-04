6d2b79fecdbb2973c27adaddd208142b61d3505d
Isabella <isabellaborin.tds1.senai@gmail.com>
2025-11-04 09:57:28 -0300

    feat: create files

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const courseRoutes = require("./src/routes/courseRoutes");
+const enrolllmentRoutes = require("./src/routes/enrollmentRoutes");
+const lessonRoutes = require("./src/routes/lessonRoutes");
+const userRoutes = require("./src/routes/userRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use("/api/courses", courseRoutes);
+app.use("/api/enrollments", enrolllmentRoutes);
+app.use("/api/lessons", lessonRoutes);
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+    console.log(`🎓 Servidor rodando em http://localhost:${PORT}`);
 });
