77fc8685ccb19e928b88957172c3da84b8afc244
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-11-05 18:17:11 -0300

    feat: add users inserts
    separados por Professores e Alunos

---
+++ src/database/insertsbea.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (photo, full_name, username, email, phone, password, role) VALUES
+('/fotoperfil', 'Maria Luisa Gimenez', 'magimenez_prof', 'maria.gimenez@edpro.edu.br', '11999990000', 'M4ria!2025xy', 'professor'),
+('/fotodeperfil', 'Luccas Augusto',    'luccasaugusto79',   'luccas.augusto@edpro.edu.br',  '11988880001', 'L4ucc4s#2025', 'professor'),
+('/fotodeperfil', 'Rafael Moretti',    'moretti.rafael',   'rafael.moretti@edpro.edu.br',  '11977770002', 'Raf3lM2025$z', 'professor'),
+('/fotodeperfil', 'Camila Duarte',     'camisduuarte',   'camila.duarte@edpro.edu.br',   '11966660003', 'C4m1la!2025ab', 'professor'),
+('/fotodeperfil', 'Gislaine Adriano',  'gislaineadriano16', 'gislaine.adriano@edpro.edu.br', '11955550004', 'G1sl4ine2025*', 'professor');
+
+INSERT INTO users (photo, full_name, username, email, phone, password, role) VALUES
+('/fotodeperfil','Mariana Alves','marialves','mariana.alves@edpro.edu.br','11933330001','M4r!ana2025xy','aluno'),
+('/fotodeperfil','Isabel Cristina Lopes','isabelac','isabela.cristina@edpro.edu.br','11933330002','Is4b3l@2025yz','aluno'),
+('/fotodeperfil','Fernanda Souza','fernandas','fernanda.souza@edpro.edu.br','11933330003','F3rn@2025abcd','aluno'),
+('/fotodeperfil','Sofia Martins','sofiam','sofia.martins@edpro.edu.br','11933330004','S0f1a!2025xz','aluno'),
+('/fotodeperfil','Clara Beatriz Oliveira','clarabeatriz','clara.beatriz@edpro.edu.br','11933330005','Cl4r@B!2025op','aluno'),
+('/fotodeperfil','Felipe Augusto','felipeaugusto','felipe.augusto@edpro.edu.br','11944440001','F3l!pe2025qw','aluno'),
+('/fotodeperfil','Diego Matheus Silva','diegomatheus','diego.matheus@edpro.edu.br','11944440002','D!3goM@2025x','aluno'),
+('/fotodeperfil','Gustavo Nunes','gustavon','gustavo.nunes@edpro.edu.br','11944440003','Gust@v0#2025','aluno'),
+('/fotodeperfil','Caio Henrique','caiohenrique','caio.henrique@edpro.edu.br','11944440004','C4ioH!2025az','aluno'),
+('/fotodeperfil','Thiago Ribeiro','thiagor','thiago.ribeiro@edpro.edu.br','11944440005','Th!ago2025jk','aluno');
+
