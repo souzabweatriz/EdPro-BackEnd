5b4ea50c033a9639d89be2579300bd7880cea4ed
maria parma <mariaparma.tds1.senai@gmail.com>
2025-11-18 11:07:48 -0300

    feat: create users inserts

---
+++ src/database/insertsmaria.sql
@@ -0,0 +1,15 @@
+INSERT INTO users (photo, full_name, username, email, phone, password, role) VALUES
+( '/.fotodeperfil', 'Rodrigo Parma', 'rodrigoparma02', 'rodrigoparma@gmail.com', '19996661071', 'rodrigop02', 'professor' ),
+( '/.fotodeperfil', 'Maria Silva', 'mariasilva', 'maria.silva@example.com', '11999990001', 'maria01', 'professor' ),
+( '/.fotodeperfil', 'João Pereira', 'joaopereira', 'joao.pereira@example.com', '11999990002', 'joao02', 'professor' ),
+( '/.fotodeperfil', 'Mariana Costa', 'marianacosta', 'mariana.costa@example.com', '21988880003', 'mariana03', 'professor' ),
+( '/.fotodeperfil', 'Carlos Alberto', 'carlosalberto', 'carlos.alberto@example.com', '31977770004', 'carlos04', 'professor' );
+
+INSERT INTO users (photo, full_name, username, email, phone, password, role) VALUES
+( '/.fotodeperfil', 'Ana Souza', 'anasouza', 'anasouza@gmail.com', '41955551234', 'ana123', 'aluno' ),
+( '/.fotodeperfil', 'Bruno Lima', 'brunolima', 'brunolima@gmail.com', '41955554321', 'bruno123', 'aluno' ),
+( '/.fotodeperfil', 'Carla Mendes', 'carlamendes', 'carlamendes@gmail.com', '41955557654', 'carla123', 'aluno' )
+( '/.fotodeperfil', 'Daniel Ferreira', 'danielferreira', 'danielferreira', '41955559876', 'daniel123', 'aluno' )
+( '/.fotodeperfil', 'Elisa Gomes', 'elisagomes', 'elisagomes@gmail.com', '41955553421', 'elisa123', 'aluno' );
+
+
