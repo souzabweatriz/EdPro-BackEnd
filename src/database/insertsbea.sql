b3d8ef78dc6ec7171fe1f4ffc81f2d1cd0ae453d
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-11-27 10:56:52 -0300

    feat: add link for photo

--- src/database/insertsbea.sql
+++ src/database/insertsbea.sql
@@ -1,16 +1,16 @@
 INSERT INTO users (photo, full_name, username, email, phone, password, role) VALUES
-('/fotoperfil', 'Maria Luisa Gimenez', 'magimenez_prof', 'maria.gimenez@edpro.edu.br', '11999990000', 'M4ria!2025xy', 'professor'),
-('/fotodeperfil', 'Luccas Augusto',    'luccasaugusto79',   'luccas.augusto@edpro.edu.br',  '11988880001', 'L4ucc4s#2025', 'professor'),
-('/fotodeperfil', 'Rafael Moretti',    'moretti.rafael',   'rafael.moretti@edpro.edu.br',  '11977770002', 'Raf3lM2025$z', 'professor'),
-('/fotodeperfil', 'Camila Duarte',     'camisduuarte',   'camila.duarte@edpro.edu.br',   '11966660003', 'C4m1la!2025ab', 'professor'),
-('/fotodeperfil', 'Gislaine Adriano',  'gislaineadriano16', 'gislaine.adriano@edpro.edu.br', '11955550004', 'G1sl4ine2025', 'professor');
+('https://pin.it/3zgRjMHMe', 'Maria Luisa Gimenez', 'magimenez_prof', 'maria.gimenez@edpro.edu.br', '11999990000', 'M4ria!2025xy', 'professor'),
+('https://pin.it/1jcrWPy72', 'Luccas Augusto',    'luccasaugusto79',   'luccas.augusto@edpro.edu.br',  '11988880001', 'L4ucc4s#2025', 'professor'),
+('https://pin.it/7rRMGgs2x', 'Rafael Moretti',    'moretti.rafael',   'rafael.moretti@edpro.edu.br',  '11977770002', 'Raf3lM2025$z', 'professor'),
+('https://pin.it/5aNwc2dPK', 'Camila Duarte',     'camisduuarte',   'camila.duarte@edpro.edu.br',   '11966660003', 'C4m1la!2025ab', 'professor'),
+('https://pin.it/4NS9IsWrt', 'Gislaine Adriano',  'gislaineadriano16', 'gislaine.adriano@edpro.edu.br', '11955550004', 'G1sl4ine2025', 'professor');
 
 INSERT INTO users (photo, full_name, username, email, phone, password, role) VALUES
-('/fotodeperfil','Mariana Alves','marialves','mariana.alves@edpro.edu.br','11933330001','M4r!ana2025xy','aluno'),
-('/fotodeperfil','Isabel Cristina Lopes','isabelac','isabela.cristina@edpro.edu.br','11933330002','Is4b3l@2025yz','aluno'),
-('/fotodeperfil','Fernanda Souza','fernandas','fernanda.souza@edpro.edu.br','11933330003','F3rn@2025abcd','aluno'),
-('/fotodeperfil','Sofia Martins','sofiam','sofia.martins@edpro.edu.br','11933330004','S0f1a!2025xz','aluno'),
-('/fotodeperfil','Clara Beatriz Oliveira','clarabeatriz','clara.beatriz@edpro.edu.br','11933330005','Cl4r@B!2025op','aluno'),
+('https://pin.it/3HATo6nh7','Mariana Alves','marialves','mariana.alves@edpro.edu.br','11933330001','M4r!ana2025xy','aluno'),
+('https://pin.it/5gHvGqz9L','Isabel Cristina Lopes','isabelac','isabela.cristina@edpro.edu.br','11933330002','Is4b3l@2025yz','aluno'),
+('https://pin.it/4uurPZWPo','Fernanda Souza','fernandas','fernanda.souza@edpro.edu.br','11933330003','F3rn@2025abcd','aluno'),
+('https://pin.it/wliGUpVas','Sofia Martins','sofiam','sofia.martins@edpro.edu.br','11933330004','S0f1a!2025xz','aluno'),
+('https://pin.it/2s776xBum','Clara Beatriz Oliveira','clarabeatriz','clara.beatriz@edpro.edu.br','11933330005','Cl4r@B!2025op','aluno'),
 ('/fotodeperfil','Felipe Augusto','felipeaugusto','felipe.augusto@edpro.edu.br','11944440001','F3l!pe2025qw','aluno'),
 ('/fotodeperfil','Diego Matheus Silva','diegomatheus','diego.matheus@edpro.edu.br','11944440002','D!3goM@2025x','aluno'),
 ('/fotodeperfil','Gustavo Nunes','gustavon','gustavo.nunes@edpro.edu.br','11944440003','Gust@v0#2025','aluno'),
